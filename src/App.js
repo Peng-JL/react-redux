e15c33b846f48947537695c4f0bf4305efdeba3b
pjl <1007020896@qq.com>
2025-12-18 14:37:40 +0800

    提交action传参

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
 React组件中修改store中的数据需要借助一个钩子函数useDispatch，它的作用是生成提交action对象的dispatch函数
 */
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './store/modules/counterStore';
+import { increment, decrement, addToNum } from './store/modules/counterStore';
 
 function App() {
   const { count } = useSelector(state => state.counter);
@@ -17,6 +17,8 @@ function App() {
       <button onClick={() => dispatch(decrement())}>-</button>
       {count}
       <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(addToNum(10))}>add to 10</button>
+      <button onClick={() => dispatch(addToNum(20))}>add to 20</button>
     </div>
   );
 }
--- src/store/modules/counterStore.js
+++ src/store/modules/counterStore.js
@@ -14,16 +14,23 @@ const counterStore = createSlice({
         },
         decrement(state) {
             state.count--
+        },
+        /*
+        提交 action 传参实现需求
+        在reducers的同步修改方法中添加action对象参数，在调用actionCreator时传递参数，该参数会被自动挂载到action对象的payload属性上。 
+        */
+        addToNum(state, action) {
+            state.count = action.payload
         }
     }
 })
 
 // 解构出来actionCreator函数
-const { increment, decrement } = counterStore.actions
+const { increment, decrement, addToNum } = counterStore.actions
 // 获取reducer
 const reducer = counterStore.reducer
 
 // 以按需导出的方式导出actionCreator
-export { increment, decrement }
+export { increment, decrement, addToNum }
 // 以默认导出的方式导出reducer
 export default reducer
